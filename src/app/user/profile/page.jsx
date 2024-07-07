d53d41af10f855f5b4efba663d73d007c60da89c
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-07-07 20:56:31 +0530

    added navbar

---
+++ src/app/user/profile/page.jsx
@@ -0,0 +1,30 @@
+"use client"
+import { useGetUserQuery } from "@/lib/services/auth";
+import { useEffect, useState } from "react";
+const Profile = () => {
+  const [user, setUser] = useState({})
+  const { data, isSuccess } = useGetUserQuery()
+  useEffect(() => {
+    if (data && isSuccess) {
+      setUser(data.user)
+    }
+  }, [data, isSuccess])
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-6 text-center">User Profile</h2>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">Name: {user.name}</label>
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">Email: {user.email}</label>
+        </div>
+        <div className="mb-4">
+          <label className="block font-medium mb-2">Verified: {user.is_verified && "Yes"}</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile
